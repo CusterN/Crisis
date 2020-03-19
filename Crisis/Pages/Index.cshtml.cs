50fc7f242f6bbda96f34daccc35ea5b009c095fe
Nicholas Custer <nicholas_custer@ahm.honda.com>
2020-03-18 21:16:31 -0400

    Added an ExportToExcel option to the index page.

--- Crisis/Pages/Index.cshtml.cs
+++ Crisis/Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Crisis.Data;
 using Crisis.Models;
+using OfficeOpenXml;
 
 namespace Crisis
 {
@@ -18,7 +19,6 @@ namespace Crisis
         {
             _context = context;
         }
-
         public IList<Supplier> Supplier { get;set; }
 
         public async Task OnGetAsync(Boolean? All)
@@ -29,6 +29,7 @@ namespace Crisis
                 Supplier = await _context.Supplier
                     .Include(s => s.Status)
                     .Include(c => c.Category)
+                    .Include(c => c.Comments)
                     .OrderByDescending(c => c.CreateDate)
                     .ToListAsync();
             }
@@ -37,9 +38,53 @@ namespace Crisis
                 Supplier = await _context.Supplier
                     .Include(s => s.Status)
                     .Include(c => c.Category)
+                    .Include(c => c.Comments)
                     .OrderByDescending(c => c.CreateDate)
                     .Where(m => m.Visible == true)
                     .ToListAsync();
+            }
+        }
+        public async Task<IActionResult> OnPostExport()
+        {
+            // Name of file
+            string sFileName = @"PartQueueRequests.xlsx";
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                // Name of worksheet(s)
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("CrisisSuppliers");
+
+                // Query table to export
+                Supplier = await _context.Supplier
+                    .Include(s => s.Status)
+                    .Include(c => c.Category)
+                    .Include(c => c.Comments)
+                    .OrderByDescending(c => c.CreateDate)
+                    .ToListAsync();
+
+                // Header of excel file
+                ws.Cells[1, 1].Value = "SupplierNo";
+                ws.Cells[1, 2].Value = "CreateDate";
+                ws.Cells[1, 3].Value = "Creator";
+                ws.Cells[1, 4].Value = "Category";
+                ws.Cells[1, 5].Value = "Status";
+                ws.Cells[1, 5].Value = "Comment";
+
+                // Body of excel file
+                int recordIndex = 2;
+                foreach (var item in Supplier)
+                {
+                    ws.Cells[recordIndex, 1].Value = item.SupplierNo;
+                    ws.Cells[recordIndex, 2].Value = item.CreateDate.ToString("yyyy-MM-dd HH:mm:ss tt");
+                    ws.Cells[recordIndex, 3].Value = item.Creator;
+                    ws.Cells[recordIndex, 4].Value = item.Category.Description;
+                    ws.Cells[recordIndex, 5].Value = item.Status.Description;
+                    ws.Cells[recordIndex, 5].Value = item.Comments.Last().Body;
+                    recordIndex++;
+                }
+
+                ws.Cells["A:AZ"].AutoFitColumns();
+                return File(pck.GetAsByteArray(), "application/octet-stream", sFileName);
             }
         }
     }
